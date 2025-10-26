f502fdfeca07f007ccaf7cc6719cedbb12c51b42
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2025-10-25 21:47:44 -0600

    Fix indentation in prime number list generation

--- lista_primos.py
+++ lista_primos.py
@@ -4,11 +4,11 @@ entrada = int(input("Escriba el número del cual quieres conocer la lista"))
 for num in range(1,entrada+1):
  es_primo = True
  for i in range(2,num):
- if num % i == 0:
- es_primo = False
- break
+  if num % i == 0:
+   es_primo = False
+   break
  if es_primo:
- lista_primos.append (num)
+  lista_primos.append (num)
 # Salidas
 print("tu lista de número primos menores que", entrada, "es ", lista_primos)
 
