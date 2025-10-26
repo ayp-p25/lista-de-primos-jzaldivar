e394b5583bdba3443d17ef26b0b865294f210c16
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2025-10-25 21:45:48 -0600

    Refactor prime number list generation logic
    Refactor the prime number list generation code to improve clarity and functionality.

--- lista_primos.py
+++ lista_primos.py
@@ -1,15 +1,15 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
+lista_primos = []
+entrada = int(input("Escriba el número del cual quieres conocer la
+lista"))
 # Proceso
-
-
+for num in range(1,entrada+1):
+ es_primo = True
+ for i in range(2,num):
+ if num % i == 0:
+ es_primo = False
+ break
+ if es_primo:
+ lista_primos.append (num)
 # Salidas
-print(salida)
+print("tu lista de número primos menores que", entrada, "es ", lista_primos)
+
